05b1dc2a8e8b86998ada2720a8be21fe1e5fc65b
Mateen Ahmad <mateen.jutt8888@gmail.com>
2024-08-27 14:42:33 +0500

    hardcoded content fix app.js

--- src/App.js
+++ src/App.js
@@ -5,32 +5,41 @@ import Avatar from "./Components/Avatar";
 import Footer from "./Components/Footer";
 import "./App.css";
 
-
 function App() {
   const HEADING_TEXT = "My Portfolio";
   const SIGNUP_BUTTON_TEXT = "Sign up";
 
+  const ABOUT_HEADING = "About Us";
+  const ABOUT_PARAGRAPH = `JavaScript, and React. I create intuitive, responsive, and dynamic
+    web applications. My focus is on writing clean, efficient code and
+    delivering engaging user experiences. With a keen eye for detail,
+    I aim to bridge the gap between design and technology. I am always
+    eager to learn and adapt to new challenges in the ever-evolving
+    field of web development. We are a team of passionate developers building awesome applications.`;
+
+  const EXPERIENCE_HEADING = "Experience";
+  const EXPERIENCE_PARAGRAPH = "Add Experience";
+
+  const AVATAR_NAME = "Mateen Ahmad";
+  const AVATAR_TITLE = "I'm a Front-End Developer";
+  const AVATAR_SKILLS = ["HTML", "CSS", "JavaScript", "Tailwind", "React.js"];
+
   return (
     <div>
       <Header heading={HEADING_TEXT} buttonText={SIGNUP_BUTTON_TEXT} />
-      <div className="container" style={{ display: "flex" }}>
+      <div className="container">
         <div>
-          <About
-            heading="About Us"
-            paragraph=" JavaScript, and React. I create intuitive, responsive, and dynamic
-              web applications. My focus is on writing clean, efficient code and
-              delivering engaging user experiences. With a keen eye for detail,
-              I aim to bridge the gap between design and technology. I am always
-              eager to learn and adapt to new challenges in the ever-evolving
-              field of web development.We are a team of passionate developers building awesome applications."
+          <About heading={ABOUT_HEADING} paragraph={ABOUT_PARAGRAPH} />
+          <Experience
+            heading={EXPERIENCE_HEADING}
+            paragraph={EXPERIENCE_PARAGRAPH}
           />
-          <Experience heading="Experience" paragraph="add Experience" />
         </div>
 
         <Avatar
-          name="Mateen Ahmad"
-          title=" I'am Front-End Developer"
-          skills={["HTML", "CSS", "JavaScript", "Tailwind", "React.js"]}
+          name={AVATAR_NAME}
+          title={AVATAR_TITLE}
+          skills={AVATAR_SKILLS}
         />
       </div>
       <Footer paragraph="Copy right 2020 " />
